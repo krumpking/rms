3d40d33642b587f4c4c1edd59dc5ddf956ea2416
Unashe Siwawa <anelesiwawa@gmail.com>
2023-10-01 19:51:40 +0200

    sms ready
    -- sms ready

--- app/api/twillioApi.ts
+++ app/api/twillioApi.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import { TWILLIO_URL } from '../constants/twillioConstants';
 import { print } from '../utils/console';
+import twilio from 'twilio';
 
 export const sendSMS = (phone: string, message: string) => {
-	print(`phone:${phone} messge ${message}`);
 	return axios.post(`${TWILLIO_URL}/api/v1/sms`, {
 		message: message,
 		phone: phone,
